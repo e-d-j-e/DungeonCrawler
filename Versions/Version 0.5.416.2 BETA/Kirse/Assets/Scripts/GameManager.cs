9c4ca306d78e8028fec27469ec386d505ae11779
xelvynx <elvynleviste@gmail.com>
2019-04-17 02:07:32 -0700

    Forge Room and Esc Menu

--- Versions/Version 0.5.416.2 BETA/Kirse/Assets/Scripts/GameManager.cs
+++ Versions/Version 0.5.416.2 BETA/Kirse/Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour {
     public GameObject startPanel;
     public GameObject escPanel;
+    public GameObject forgePanel;
     bool active = false;
     private static GameManager _instance;
     public static GameManager gm
@@ -21,10 +22,12 @@ public class GameManager : MonoBehaviour {
             return _instance;
         }
     }
+    
     //public int token = 0;
     public TextMeshProUGUI actionText;
     public TextMeshProUGUI forgeable;
 
+    
 
     // public Text tokenText;
     //public void TokenUpdate(int i)
@@ -39,11 +42,19 @@ public class GameManager : MonoBehaviour {
     //}
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && startPanel.activeInHierarchy == false)
+        if (Input.GetKeyDown(KeyCode.Escape) && startPanel.activeInHierarchy == false && forgePanel.activeInHierarchy == false && active == false)
         {
             escPanel.SetActive(true);
-            
+            active = true;
+            Time.timeScale = 0;
         }
+        else if(Input.GetKeyDown(KeyCode.Escape) && active == true) //startPanel.activeInHierarchy == false && forgePanel.activeInHierarchy == false && escPanel.activeInHierarchy == true)
+        {
+            escPanel.SetActive(false);
+            active = false;
+            Time.timeScale = 1;
+        }
+
     }
 
     public void SlowMo()
