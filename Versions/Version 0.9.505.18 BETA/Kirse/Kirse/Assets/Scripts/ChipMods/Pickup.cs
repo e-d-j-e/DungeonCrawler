c65608a6b5b8a2efcf23cb8dfcbffdc2d818594c
suborange <suborange@users.noreply.github.com>
2019-05-05 21:59:36 -0700

    added temp minimap, shows enemies around

---
+++ Versions/Version 0.9.505.18 BETA/Kirse/Kirse/Assets/Scripts/ChipMods/Pickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+    private ModInv mI;
+    public GameObject modPrefab;
+
+	// Use this for initialization
+	private void Start () {
+        mI = GameObject.FindGameObjectWithTag("Player").GetComponent<ModInv>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            for (int i = 0; i < mI.modSlots.Length; i++)
+            {
+                if(mI.isFull[i]==false)
+                {
+                    mI.isFull[i] = true;
+                    Instantiate(modPrefab, mI.modSlots[i].transform, false);
+                    Destroy(gameObject);
+                    break; 
+                }
+
+            }
+        }
+    }
+}
