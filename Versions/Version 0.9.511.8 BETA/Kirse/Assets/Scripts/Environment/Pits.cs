95ce87505bdfcb4f54a0a929d359343bdee5ac57
suborange <suborange@users.noreply.github.com>
2019-05-11 19:50:40 -0700

    more needed changes
    added roborange discard, pauses screen, end screen, fiex other small stuff

---
+++ Versions/Version 0.9.511.8 BETA/Kirse/Assets/Scripts/Environment/Pits.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pits : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 ppos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject==player)
+        {
+            ppos = player.transform.position;
+
+        }
+    }
+}
---
+++ Versions/Version 0.9.511.8 BETA/Kirse/Assets/Scripts/Player/Slash.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slash : MonoBehaviour {
+    //public int damage = 65;
+    //private tokens toke = new tokens();
+
+
+    private void OnTriggerEnter2D(Collider2D att)
+    {
+        enemyAI enemy = att.GetComponent<enemyAI>();
+        //toke.incToken();
+
+        if (enemy != null)
+        {
+            if (this.tag=="slash")
+            {
+                //if (enemy.name == "Rock") //REDUCED DAMAGE
+                //{
+                //    enemy.takeDamage(5);
+                //    FindObjectOfType<AudioManager>().Play("Slash");
+                //}
+                //else
+                //{
+                    enemy.takeDamage(65);
+                    FindObjectOfType<AudioManager>().Play("Slash");
+                //}
+
+            }
+            else if (this.tag=="spinslash")
+            {
+                //enemy.takeDamage(120);
+                FindObjectOfType<AudioManager>().Play("Slash");
+            }
+        }
+    }
+}
